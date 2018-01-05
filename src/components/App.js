1dcfa9720f7678c76b00aba209fadf3dc4d49aef
Lakshmi <lakshmi.nishthala@gmail.com>
2018-01-05 08:43:08 -0800

    Initial setup done

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import "../App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </Header>
+        <PageContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
